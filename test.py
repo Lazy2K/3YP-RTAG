dfd0537b91ecf80e9d7e34111bb297bfb671e578
Douglas <kekblob@gmail.com>
2024-01-25 23:37:30 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 import serial
 import sys
 
+GPGGA = ""
+NMEA = ""
+
 ser = serial.Serial("/dev/ttyS0")
 while True:
     rc = ser.readline()
-    print(rc)
+    if "$GPGGA" in rc:
+        GPGGA = rc.split("$GPGGA,", 1)[1]
+    print(GPGGA)

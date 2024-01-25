6145da595841e34452058712ac32b6431791649c
Douglas <kekblob@gmail.com>
2024-01-25 23:54:33 +0000

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ while True:
     rc = ser.readline()
     da = rc.find("$GPGGA,".encode())
     print(da)
-    if da:
+    if da > 0:
         GPGGA = rc.split("$GPGGA,".encode(), 1)[0]
         NMEA = GPGGA.split(",".encode())
         print(NMEA)

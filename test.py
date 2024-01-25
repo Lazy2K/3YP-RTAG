1a8631904855fda8674fd1a56f86b9c984d5e49f
Douglas <kekblob@gmail.com>
2024-01-25 23:39:07 +0000

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,6 @@ NMEA = ""
 ser = serial.Serial("/dev/ttyS0")
 while True:
     rc = ser.readline()
-    if "$GPGGA" in rc:
+    if rc.find("$GPGGA"):
         GPGGA = rc.split("$GPGGA,", 1)[1]
     print(GPGGA)

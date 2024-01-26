358d42de3ee0ab15b3a9d844aab447776a6b0a7b
Douglas <kekblob@gmail.com>
2024-01-26 19:03:57 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
+import pynmea2
 import serial
 import sys
+
 
 GPGGA = []
 NMEA = []
@@ -9,9 +11,9 @@ GPGGA_look = "$GPGGA,"
 ser = serial.Serial("/dev/ttyS0")
 while True:
     rc = ser.readline()
-    print(rc)
-    da = rc.find(GPGGA_look)
-    print(rc)
+    if rc[0:6] == "$GPRMC":
+        msg = pynmea2.parse(rc)
+        print(msg)
     if da > 0:
         GPGGA = rc.split("$GPGGA,".encode(), 1)[0]
         NMEA = GPGGA.split(",".encode())

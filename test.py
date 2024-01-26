89d329883bdd1573632e5efd6f9f0012c4243541
Douglas <kekblob@gmail.com>
2024-01-26 00:02:26 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 import serial
 import sys
 
-GPGGA = 0
-NMEA = 0
+GPGGA = []
+NMEA = []
+
+GPGGA_look = "$GPGGA,"
 
 ser = serial.Serial("/dev/ttyS0")
 while True:
     rc = ser.readline()
-    da = rc.find("$GPGGA,".encode())
+    da = rc.find(GPGGA_look)
     print(rc)
     if da > 0:
         GPGGA = rc.split("$GPGGA,".encode(), 1)[0]

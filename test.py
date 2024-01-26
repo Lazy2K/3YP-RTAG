630a7a9b31f9b8151164e937fe06e6e6b27f1680
Douglas <kekblob@gmail.com>
2024-01-26 19:08:55 +0000

    Update test.py

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ ser = serial.Serial("/dev/ttyS0")
 while True:
     rc = ser.readline()
     print(rc)
-    if rc[0:6] == "$GPGGA":
+    if rc[0:6] == "$GPGGA".econde():
         print("Triggered")
         msg = pynmea2.parse(rc)
         print(msg)

881ebdf72dd928047a491091d3481fb13b98c8c7
Douglas <kekblob@gmail.com>
2024-01-26 19:20:03 +0000

    Update test.py

--- test.py
+++ test.py
@@ -11,9 +11,8 @@ GPGGA_look = "$GPGGA,"
 ser = serial.Serial("/dev/ttyS0")
 while True:
     rc = ser.readline()
-    print(rc)
     if rc[0:6] == "$GPGGA".encode():
         print(rc.decode())
         msg = pynmea2.parse(rc.decode())
-        print(msg.fields)
-        print(msg.gps_qual)
+        if msg.gps_qual <= 0:
+            print("Awaiting GPS connection")

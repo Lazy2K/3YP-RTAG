d9b1218ad6acd36734918b553a26a9f15be53ef7
Douglas <kekblob@gmail.com>
2024-01-29 09:56:29 +0000

    Update vehicle.py

--- modules/vehicle/vehicle.py
+++ modules/vehicle/vehicle.py
@@ -26,6 +26,7 @@ class HardwareInterface:
                     return None
                 serialLine = self.serialConnection.readline()
                 print(serialLine)
+                print(self.GPGGA.encdoe())
                 if serialLine[0:6] == self.GPGGA.encode():
                     gpsData = pynmea2.parse(serialLine.decode())
                     return gpsData

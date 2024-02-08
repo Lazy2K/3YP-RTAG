7bd9494dff21847b71f135cc3db2c9709c113e2e
Douglas <kekblob@gmail.com>
2024-02-08 21:33:48 +0000

    Update acc.py

--- acc.py
+++ acc.py
@@ -11,8 +11,7 @@ int1 = digitalio.DigitalInOut(board.D24)
 lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, int1=int1)
 
 
-x, y, z = lis3dh.acceleration
-
 while True:
+    x, y, z = lis3dh.acceleration
     print("%0.3f %0.3f %0.3f" % (x, y, z))
     time.sleep(2)

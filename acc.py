f949436834c5da04c6735ed4604a1b31fb181091
Douglas <kekblob@gmail.com>
2024-02-08 21:22:12 +0000

    Update acc.py

--- acc.py
+++ acc.py
@@ -6,6 +6,6 @@ import adafruit_adxl34x
 i2c = busio.I2C(board.SCL, board.SDA)
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
 
-# while True:
-# print("%f %f %f" % accelerometer.acceleration)
-# time.sleep(0.5)
+while True:
+    print("%f %f %f" % accelerometer.acceleration)
+    time.sleep(0.5)

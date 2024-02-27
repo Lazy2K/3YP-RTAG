835ec3fa7a62bfea04e274e91ef0163e07b31dbd
Douglas <kekblob@gmail.com>
2024-02-27 16:22:42 +0000

    Update detector.py

--- modules/detector/detector.py
+++ modules/detector/detector.py
@@ -10,7 +10,7 @@ class Detector:
         self.vehicle = vehicle
         self.generator = generator
 
-        self.timeOutSeconds = 5
+        self.timeOutSeconds = 5000
         self.timeTillNext = {  # This seems like a bad solution and inefficient
             "LANE_CHANGE": time.time(),
             "SPEED_LIMIT_EXCEEDED": time.time(),

e6c3fd528c2f8f18025eca9d05c9724a5b7f5241
Douglas <kekblob@gmail.com>
2024-02-27 16:05:11 +0000

    .

--- modules/detector/detector.py
+++ modules/detector/detector.py
@@ -1,11 +1,14 @@
 from ..vehicle.vehicle import Vehicle
+from ..generator.generator import Generator
+from ..generator.generator import Alert, alertType, alertSound
 
 
 class Detector:
-    def __init__(self, vehicle: Vehicle, generator):
+    def __init__(self, vehicle: Vehicle, generator: Generator):
         self.vehicle = vehicle
         self.generator = generator
 
     def run(self):
-        if self.vehicle.zAcceleration > 0:
-            print(self.vehicle.zAcceleration)
+        if self.vehicle.zAcceleration > 0.5:
+            self.generator.registerAlert(
+                Alert(alertType.ACCELERATION_TOO_FAST, alertSound.ACCELERATION_TOO_FAST))

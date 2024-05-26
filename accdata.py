6ea08e3b23e9171ce8be0c9753530c1acdd14a01
Douglas <kekblob@gmail.com>
2024-05-26 15:06:48 +0100

    Create accdata.py

---
+++ accdata.py
@@ -0,0 +1,24 @@
+import busio
+import digitalio
+import adafruit_lis3dh
+import board
+import time
+
+
+class Accelerometer:
+    """ Docstring """
+
+    def __init__(self):
+        self.I2C = busio.I2C(board.SCL, board.SDA)
+        self.int1 = digitalio.DigitalInOut(board.D24)
+        self.acceleromiter = adafruit_lis3dh.LIS3DH_I2C(
+            self.I2C, int1=self.int1)
+
+    def collectAccData(self):
+        return self.acceleromiter.acceleration
+
+
+acc = Accelerometer()
+
+for i in range(0, 1000):
+    print(acc.collectAccData())

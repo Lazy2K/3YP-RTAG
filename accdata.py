fd69c6d5eef114969bae4a6273e885257fe296a6
Douglas <kekblob@gmail.com>
2024-05-26 15:15:15 +0100

    Update accdata.py

--- accdata.py
+++ accdata.py
@@ -26,6 +26,6 @@ with open('data.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=' ',
                         quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
-    for i in range(0, 1000):
+    while True:
         data = acc.collectAccData()
         writer.writerow([data.x, data.y, data.z])
